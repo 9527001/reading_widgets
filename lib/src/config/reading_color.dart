db9f0a80768ed71bc3e6e61c14d9432fb0db461d
zhuyouzhong <1377092849@qq.com>
2019-11-18 09:58:27 +0800

    图片添加

--- lib/src/config/reading_color.dart
+++ lib/src/config/reading_color.dart
@@ -57,10 +57,6 @@ class RdColors{
   static const Color COLOR_404040 = Color(0xff404040);
   static const Color COLOR_B6B6B6 = Color(0xffb6b6b6);
 
-//  static const Color COLOR_2470AF = Color(0xff2470AF);
-//  static const Color COLOR_3CA7FB = Color(0xff3CA7FB);
-//  static const Color COLOR_92D1FC = Color(0xff92D1FC);
-//  static const Color COLOR_FFA80A = Color(0xffFFA80A);
   static const Color COLOR_E4F4FE = Color(0xffE4F4FE);
   static const Color COLOR_199CFF = Color(0xff199CFF);
   static const Color COLOR_4DB2FF = Color(0xff4DB2FF);
